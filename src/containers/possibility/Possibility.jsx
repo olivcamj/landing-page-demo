045c2a777d8f08e1d817f50179fafba910a0b75a
Olivia Cameron <olivia.cameronj15@gmail.com>
2024-12-05 16:31:40 -0500

    feat: possiblity section (stopping point)

--- src/containers/possibility/Possibility.jsx
+++ src/containers/possibility/Possibility.jsx
@@ -1,10 +1,19 @@
 import React from 'react';
+import possibilityImage from '../../assests/possibility.png';
 import './possibility.css';
 
 const Possibility = () => {
   return (
-    <div>
-      
+    <div className="gpt3__possibilty section__margin" id="possibility">
+      <div className="gpt3__possibility-image">
+        <img src={possibilityImage} alt="possibility"/>
+      </div>
+      <div>
+        <h4>Request Early Access to Get Started</h4>
+        <h1 className="gradient__text">The possiblities are beyond your imagination</h1>
+        <p></p>
+        <p>Request Early Access to Get Started </p>
+      </div>
     </div>
   )
 }
