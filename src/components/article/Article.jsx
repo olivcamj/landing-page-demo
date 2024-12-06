fafd04189c9ac93b30280cc968109c0946914c4e
Olivia Cameron <olivia.cameronj15@gmail.com>
2024-12-06 11:19:38 -0500

    feat: article section complete

--- src/components/article/Article.jsx
+++ src/components/article/Article.jsx
@@ -1,9 +1,20 @@
 import React from 'react';
 import './article.css';
 
-const Article = () => {
+const Article = ({ imgUrl, date, title }) => {
   return (
-    <div>Article</div>
+    <section className="gpt3__blog-container_article">
+      <div className="gpt3__blog-container_article">
+        <img src={imgUrl} alt="blog" />
+      </div>
+      <div className="gpt3__blog-conteinaer_article-content">
+        <div>
+          <p>{ date} </p>
+          <h3>{ title}</h3>
+        </div>
+      </div>
+      <p>Read Full Article</p>
+    </section>
   )
 }
 
