fa7b491c7c5afa8e595e432428dc85427b198862
Olivia Cameron <olivia.cameronj15@gmail.com>
2024-12-05 22:39:56 -0500

    fix: replace element and rename css class

--- src/containers/possibility/Possibility.jsx
+++ src/containers/possibility/Possibility.jsx
@@ -4,15 +4,15 @@ import './possibility.css';
 
 const Possibility = () => {
   return (
-    <div className="gpt3__possibilty section__margin" id="possibility">
-      <div className="gpt3__possibility-image">
-        <img src={possibilityImage} alt="possibility"/>
+    <div className="gpt3__possibility section__margin" id="possibility">
+      <div className="gpt3__possibility_image-container">
+        <img src={possibilityImage} alt="possibility" className="gpt3__possibility-image"/>
       </div>
-      <div>
+      <div className="gpt3__possibility-content">
         <h4>Request Early Access to Get Started</h4>
         <h1 className="gradient__text">The possiblities are beyond your imagination</h1>
         <p></p>
-        <p>Request Early Access to Get Started </p>
+        <h4>Request Early Access to Get Started </h4>
       </div>
     </div>
   )
