b4d08bc5815c2b4c5e540f686b86ca0b735fe3b3
Olivia Cameron <olivia.cameronj15@gmail.com>
2024-12-06 11:19:00 -0500

    feat: group images to import / more concise

---
+++ src/containers/blog/imports.js
@@ -0,0 +1,13 @@
+import blog1 from '../../assests/blog1.png';
+import blog2 from '../../assests/blog2.png';
+import blog3 from '../../assests/blog3.png';
+import blog4 from '../../assests/blog4.png';
+import blog5 from '../../assests/blog5.png';
+
+export {
+  blog1,
+  blog2,
+  blog3,
+  blog4,
+  blog5
+}
