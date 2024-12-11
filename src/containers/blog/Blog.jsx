c9ee35fe7025a93fbcf4cbc43d9b0ee07aee7e2c
Olivia Cameron <olivia.cameronj15@gmail.com>
2024-12-11 12:11:17 -0500

    fix: rename css classes

--- src/containers/blog/Blog.jsx
+++ src/containers/blog/Blog.jsx
@@ -5,15 +5,15 @@ import './blog.css';
 
 const Blog = () => {
   return (
-    <section className=" gpt3__blog section__margin"id="blog">
-      <div className="gpt3__heading">
-        <h1 className="gradient__text"></h1>
+    <section className="blog section__margin"id="blog">
+      <div className="blog__heading">
+        <h1 className="blog__title gradient__text">Alot is happening, we are blogging about it. </h1>
       </div>
-      <div className="gpt3__blog-container">
-        <div className="gpt3__blog-group-a">
+      <div className="blog__container">
+        <div className="blog__group-a">
           <Article imgUrl={blog1} title="GPT-3 and Open AI is the future. Let's explore how?" date="Sept 26, 2021" />
         </div>
-        <div className="gpt3__blog-group-b">
+        <div className="blog__group-b">
         <Article imgUrl={blog2} title="GPT-3 and Open AI is the future. Let's explore how?"date="Sept 26, 2021" />
         <Article imgUrl={blog3} title="GPT-3 and Open AI is the future. Let's explore how?" date="Sept 26, 2021" />
         <Article imgUrl={blog4} title="GPT-3 and Open AI is the future. Let's explore how?" date="Sept 26, 2021" />
